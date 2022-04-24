6c6d736f18d591403ee52038a68fc2fb5adeff8c
AlexeiDenis <83495616+AlexeiDenis@users.noreply.github.com>
2022-04-24 22:48:58 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,32 @@
+let img = document.getElementById('imag');
+let btn1 = document.getElementById('btn1');
+let btn2 = document.getElementById('btn2');
+let btn3 = document.getElementById('btn3');
+let btns = document.querySelectorAll('button');
+btns.forEach((e)=>{
+e.addEventListener('click',()=>{
+    img.animate(
+        [
+            // keyframes
+            { opacity: "0.5" },
+            { opacity: "1" }
+        ],
+        {
+            // timing options
+            duration: 850,
+            iterations: 1,
+            easing: "ease-out"
+        }
+    );
+})
+})
+btn3.addEventListener('click', () => {
+    
+    img.src = "./imagini/image4.webp"
+});
+btn1.addEventListener('click', () => {
+     img.src = "./imagini/image3.webp"
+});
+btn2.addEventListener('click', () => {
+     img.src = "./imagini/image1.webp"
+});
